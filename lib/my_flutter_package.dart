85ba10b5c6075655757bb584a11c06a59829c58c
Soun Savdan <soun.savdanit@gmail.com>
2024-12-13 10:22:28 +0700

    first commit

---
+++ lib/my_flutter_package.dart
@@ -0,0 +1,3 @@
+library my_flutter_package;
+
+export 'src/my_app.dart';
---
+++ lib/src/my_app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ShadowContainer extends StatefulWidget {
+  const ShadowContainer({super.key});
+
+  @override
+  State<ShadowContainer> createState() => _ShadowContainerState();
+}
+
+class _ShadowContainerState extends State<ShadowContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      height: 200,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.red,
+        boxShadow: const [
+          BoxShadow(
+            blurRadius: 1,
+            spreadRadius: 2,
+          )
+        ],
+      ),
+    );
+  }
+}
